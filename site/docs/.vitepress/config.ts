79108084a3d429803c4fc2273c0727cad61597da
xuhongliWeb <564507650@qq.com>
2023-05-16 10:11:50 +0800

    ci 打包

--- site/docs/.vitepress/config.ts
+++ site/docs/.vitepress/config.ts
@@ -6,7 +6,7 @@ import { mdPlugin } from './config/plugins'
 
 export default defineConfig({
   title: 'vxx UI',
-  base: process.env.NODE_ENV === 'production' ? '/docs/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   head: [
 
     [
